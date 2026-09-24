fc5f455a8a384a0b5d4044207383860c5f138da1
Andres Marquez <andresmr955@gmail.com>
2026-09-24 09:01:19 -0400

    adding intro and expresions

---
+++ b_Expressions_Operations/expression.py
@@ -0,0 +1,39 @@
+# Une expression est un combinasion d'opérateurs, de valeurs, de variables et d'appels de sousalgorithmes
+print(1 + 2 + 3)
+print(1 + 2 * 3)
+print((1 + 2) * 3)
+
+#Dans presque tous les langages de programation un réel et nul sont vus comme CONSTATNTE et sa valeur est équivalente au nombre lui-même
+#LES ENTIERS (int)
+print(5, 'Entier positif')
+print( -42, 'Entier negarif')
+print(0, 'zéro')
+
+#LES FLOTTANTS
+print(3.14159, 'nombre décimal (flottant)')
+print(2.71828, 'Autre flottant')
+print(-0.5)
+
+#!!ON DOIT ÉCRIRE LES DÉCIMALS AVEC DES POINTS ET NON AVEC VERGULE
+
+#NOTATION SCIENTIFIQUE
+#Est un manière de représenter les nombres très grans ou très petits de manière compacte et lisible.
+
+
+print(3.14e5, 'Équivalent à 314000')
+print(3.5e-3, 'Équivalent à 0.0025')
+print(6.022e23, 'Équilent à une mole')
+
+#Pour les nombres >= 10e15, l'affichege se fait avec la représentation sicentifique.
+
+#OPERATEURS ARITHMÉTIQUES
+# | Symbole | Description |
+# |   +     | Additionne  |
+# |   -     | Soustrait   |
+# |   *     | Multiplie   |
+# |   /     | Divise      |
+# |   ||    | Divise et garde seulment la partie entière|
+# |   %     | Reste de la division|
+# |   **    | Exponentiation(Puissance)|
+
+
